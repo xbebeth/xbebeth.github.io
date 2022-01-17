2af71d09f7bb29fd819358fc98ae034f0f27601c
xbebeth <97406062+xbebeth@users.noreply.github.com>
2022-01-17 17:08:14 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function openNav() {
+  var x = document.getElementById("mobile-nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}

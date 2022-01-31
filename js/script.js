fb346f4ba359f91c9df4ec833a99a9a764fcb392
Elisabeth <e.gamachelavoie@gmail.com>
2022-01-30 23:26:00 -0500

    pushes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Code for the mobile nav
 function openNav() {
   var x = document.getElementById("mobile-nav");
   if (x.style.display === "block") {
@@ -5,4 +6,30 @@ function openNav() {
   } else {
     x.style.display = "block";
   }
+}
+
+//Code for the detail windows
+function showKomo(){
+  document.getElementById('allcontent').style.opacity="0.3";
+  document.getElementById('detwin').style.display="block";
+}
+function showAtbr(){
+  document.getElementById('allcontent').style.opacity="0.3";
+  document.getElementById('detwin').style.display="block";
+}
+function showCamc(){
+  document.getElementById('allcontent').style.opacity="0.3";
+  document.getElementById('detwin').style.display="block";
+}
+function showLotf(){
+  document.getElementById('allcontent').style.opacity="0.3";
+  document.getElementById('detwin').style.display="block";
+}
+function showVpos(){
+  document.getElementById('allcontent').style.opacity="0.3";
+  document.getElementById('detwin').style.display="block";
+}
+function closeme(){
+  document.getElementById('detwin').style.display="none";
+  document.getElementById('allcontent').style.opacity="1";
 }
